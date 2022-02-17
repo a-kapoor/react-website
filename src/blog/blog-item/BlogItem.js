a5e8e205753266187361c0530011c21b30b96c58
Anshul <itsanshulkapoor@gmail.com>
2022-02-17 11:08:29 -0500

    update css of blog post

--- src/blog/blog-item/BlogItem.js
+++ src/blog/blog-item/BlogItem.js
@@ -34,7 +34,19 @@ export default function BlogItem() {
             <div class="flex relative z-20 items-center">
                 <div class="container mx-auto px-6 flex flex-col justify-between items-center relative py-4">
                     <div class="flex flex-col">
-                        {<ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight, rehypeRaw]}></ReactMarkdown>}
+                        <ReactMarkdown children={markdown}
+                            remarkPlugins={[remarkGfm]}
+                            rehypePlugins={[rehypeHighlight, rehypeRaw]}
+                            components={{
+                                pre: ({node, ...props}) => <pre className="mb-7 px-7 py-7 bg-gray-100 rounded-md" {...props} />,
+                                p: ({node, ...props}) => {
+                                    return <p className="pb-7" {...props} />
+                                },
+                                h1: ({node, ...props}) => {
+                                    return <h1 className="text-3xl my-6 text-center dark:text-white" {...props} />
+                                },
+                                h2: ({node, ...props}) => <h1 className="text-1xl my-2 dark:text-white" {...props} />
+                              }}/>
                     </div>
                 </div>
             </div>
